445ca03625bc7e96c9ef4d94e7387d12c40747df
daniela risco <driscop@gmail.com>
2017-05-12 09:44:30 -0300

    ejercicio terminado sin funcionar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,23 @@
-document.getElementById("toggle").addEventListener("click",function(event){
- event.preventDefault();
+document.getElementById("toggle").addEventListener("click", function(event){
+	event.preventDefault();
  document.getElementById("nav-header").classlist.toggle("open");
  document.getElementById("body").classlist.toggle("overflow-hidden");
  });
-
+var lastScrollTop = 0;
+window.addEventListener("scroll", function(){
+    var currentScroll = window.pageYOffset || document.body.scrollTop;
+    if (currentScroll > lastScrollTop){
+       document.getElementById("nav-header").classList.remove("header-in");
+       document.getElementById("nav-header").style.opacity = 0;
+    } else {
+        document.getElementById("nav-header").classList.add("header-in");
+        document.getElementById("nav-header").classList.add("solid");
+        document.getElementById("nav-header").style.opacity = 1;
+        if (currentScroll <= 3){
+            document.getElementById("nav-header").classList.remove("solid");
+            document.getElementById("nav-header").classList.remove("header-in");
+        }
+    }
+    lastScrollTop = currentScroll;
+}, false);
 
